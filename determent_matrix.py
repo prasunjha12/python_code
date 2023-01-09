5adbfd6ed2ed465d84ccca7c1de2d50047bbd0ff
Prasun <Jhaprasun12@gmail.com>
2023-01-09 20:30:15 +0530

    remove unwanted comments

--- determent_matrix.py
+++ determent_matrix.py
@@ -4,10 +4,7 @@ def getcofactor(m, i, j):
   
 def determinantOfMatrix(mat):
  
-    # if given matrix is of order
-    # 2*2 then simply return det
-    # value by cross multiplying
-    # elements of matrix.
+   
     if(len(mat) == 2):
         value = mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1]
         return value
@@ -19,18 +16,12 @@ def determinantOfMatrix(mat):
     # of matrix a.
     for current_column in range(len(mat)):
  
-        # calculating the sign corresponding
-        # to co-factor of that sub matrix.
         sign = (-1) ** (current_column)
  
-        # calling the function recursily to
-        # get determinant value of
-        # sub matrix obtained.
+       
         sub_det = determinantOfMatrix(getcofactor(mat, 0, current_column))
  
-        # adding the calculated determinant
-        # value of particular column
-        # matrix to total Sum.
+      
         Sum += (sign * mat[0][current_column] * sub_det)
  
     # returning the final Sum
